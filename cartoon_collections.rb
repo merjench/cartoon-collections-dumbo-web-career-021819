2ce3ef5057cc3973b766b97b5e4a49d007eacf31
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-12 23:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,12 @@ def long_planeteer_calls# code an argument here
 end
 
 def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  cheeses = ["gouda", "cheddar", "camembert"]
+  array.find do |item|
+    cheeses.include?(item)
+  end
+end
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
 end
